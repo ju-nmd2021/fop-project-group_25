30e9b6d03162d9f6746ae553390d03d61d15d261
Andreea Rus <93585146+andreearrus@users.noreply.github.com>
2022-05-04 11:38:00 +0200

    Planning

---
+++ Andreea.js
@@ -0,0 +1,12 @@
+// canva setup 900x600px
+// pixel look for avatar, simple shape
+// size of the characters: 50x50
+// Changing world with space bar
+
+push();
+fill(255);
+rect(0, 0, 900, 600);
+pop();
+
+fill(0);
+rect(50, 50, 50, 50);
