82333d49510c919998519e5cea5bf0e123a8f060
Andreea Rus <93585146+andreearrus@users.noreply.github.com>
2022-05-09 14:34:16 +0200

    the character is moving right or left

--- Andreea.js
+++ Andreea.js
@@ -1,7 +1,11 @@
 //standard function to initialize the canvas (a 900x600 square will be shown)
+let y;
+let x;
 function setup() {
   createCanvas(900, 600);
   background(0);
+  x = 0;
+  y = 0;
 }
 
 // platform 1 and platform 2 object (this will be two rectagles on the button of the screen)
@@ -19,10 +23,49 @@ let platformTwo = {
   h: 300,
 };
 
+//  the character intial point, it will fall when the level is starting
+
+let character = {
+  x: 100,
+  y: 50,
+  w: 50,
+  h: 50,
+};
+
 function draw() {
+  // platform
   push();
   fill(255);
   rect(platformOne.x, platformOne.y, platformOne.w, platformOne.h);
   rect(platformTwo.x, platformTwo.y, platformTwo.w, platformTwo.h);
+
+  // character
+
+  rect(x + character.x, y + character.y, character.w, character.y);
   pop();
+
+  if (keyIsPressed) {
+    if (key == "ArrowRight") {
+      x = x + 1;
+    }
+  }
+
+  if (keyIsPressed) {
+    if (key == "ArrowLeft") {
+      x = x - 1;
+    }
+  }
+
+  if (keyIsPressed) {
+    if (key == "ArrowUp") {
+      y = y - 1;
+    }
+  }
 }
+
+// defining constants for the boundaries of the platforms
+const pltformOneHorizontalLimitLeft = 100;
+const pltformOneHorizontalLimitRight = 350;
+
+const pltformTwoHorizontalLimitLeft = 550;
+const pltformTwoHorizontalLimitRight = 800;
